86cf4c8b0e433f2c2cbb9566afe61de34fdfc8e5
flsouto <fabiolimasouto@gmail.com>
2022-12-29 14:09:10 -0300

    updates

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 return [
     'bkg_imgs' => '',
     'sym_wavs' => "/home/fabio/Documents/cinematic1/saved*/"."*.wav",
-    'drm_wavs' => __DIR__."/gen22/{,*saved/}*.wav",
+    'drm_wavs' => __DIR__."/noismk/gen22/{,*saved/}*.wav",
     'smp_path' => '/home/fabio/Documents/sampler/FlSouto/Sampler2.php'
 ];
---
+++ mod.php
@@ -0,0 +1,37 @@
+<?php
+use FlSouto\Sampler;
+$config = require(__DIR__."/config.php");
+require_once($config['smp_path']);
+
+if(empty($argv[4])){
+    die("Usage: cmd <dir> <index> <type> <args>");
+}
+
+$dir = $argv[1];
+$index = $argv[2];
+$type = $argv[3];
+$args = implode(' ',array_slice($argv,4));
+
+$target = "$dir/{$index}_$type.wav";
+$a = new Sampler($target);
+$a->mod($args);
+$a->save($target);
+
+if($type == 'sym'){
+    $b = "$dir/{$index}_drm.wav";
+} else {
+    $b = "$dir/{$index}_sym.wav";
+}
+
+$b = new Sampler($b);
+$ab = $b()->mix($a,false);
+$ab->save("$dir/{$index}_sym_drm.wav");
+
+if($type == 'sym'){
+    $track = [$a, $ab, $b];
+} else {
+    $track = [$b, $ab, $a];
+}
+
+$track = $track[0]()->add($track[1])->add($track[2]);
+$track->save("$dir/{$index}_track.wav");
